f02463ca9b4a5063a7fd710e9b7a1935859c9073
Nelson Pino <nelson.pino.net@gmail.com>
2021-05-07 16:05:42 -0400

    configuracion de react webpack y babel

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./src/app/index.js",
+  output: {
+    path: __dirname + "/src/public",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+};
