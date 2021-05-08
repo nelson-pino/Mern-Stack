662d69bd3b614f7e859aad7ad44c2bffc373f2a3
Nelson Pino <nelson.pino.net@gmail.com>
2021-05-07 23:22:53 -0400

    trabajos con el formulario de tasks

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  addTask(e) {
+    console.log("agregando tarea");
+    e.preventDefault();
+  }
+
   render() {
-    return <h1>Componente Reactivo de React</h1>;
+    return (
+      <div>
+        <nav className="light-blue darken-4">
+          <div className="container">
+            <a href="/" className="brand-logo">
+              Mern Stack
+            </a>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col s5">
+              <div className="card">
+                <div className="card-content">
+                  <form onSubmit={this.addTask}>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <input type="text" placeholder="Titulo de la tarea" />
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <textarea
+                          placeholder="Descripcion de la Tarea"
+                          className="materialize-textarea"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <button type="submit" className="btn light-blue darken-4">
+                      Guardar
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="col s7">2</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
